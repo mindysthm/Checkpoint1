810456bb60df33c9f81975c4b7296af6d937cb0e
mindysthm <mindysetham@gmail.com>
2024-11-07 19:31:07 +0100

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -5,16 +5,23 @@ if [ $# -eq 0 ] ; then
    echo "Il manque les noms d'utilisateurs en argument - Fin du script"
     exit 1 
   fi
-while
+
+args=$@
+for utilisateur in $args
+
 do
 
- if  [$* etc/passwd ] ; then
+ if  grep -q = "^$utilisateur: /etc/passwd; then
  echo "L'utilisateur $* existe déjà"
+ else
+ id sudo useradd $utilisateur; then
+ echo "L'utilisateur $utilisateur a été créé"
+ else
+ echo "erreur"
+ fi
 
-else
+ fi
 
-adduser $*
-echo "l'utilisateur $* existe déjà" 
-fi
+ done
 
-done
+
